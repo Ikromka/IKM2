40ac1ae75b74df4d964b17b361492a31a1229982
Ikromka <loletka.kotletka@gmail.com>
2025-06-25 23:14:15 +0500

    Сделал функцию нахождения последнего символа и кусок бэктрэкинга

--- IKM2/functions.cpp
+++ IKM2/functions.cpp
@@ -1,1 +1,27 @@
 #include "functions.h"
+
+wchar_t LastChar(wstring Word) {
+	const int n = Word.size();
+	if (Word[n - 1] == L'ü' || Word[n - 1] != L'ú' || Word[n - 1] != L'û') {
+		return Word[n - 2];
+	}
+	else {
+		return Word[n - 1];
+	}
+}
+
+void Backtracking(const vector<wstring>& Words, vector<bool>& WasUsed, wchar_t PrevChar) {
+	const int n = Words.size();
+	int WordsUsed = 0;
+	for (int i = 0; i < n; i++) {
+		if (WasUsed[i]) {
+			WordsUsed++;
+		}
+		else if (Words[i][0] == PrevChar) {
+			WasUsed[i] = true;
+			Backtracking(Words, WasUsed, LastChar(Words[i]));
+			WasUsed[i] = false;
+		}
+	}
+	
+}
--- IKM2/functions.h
+++ IKM2/functions.h
@@ -1,3 +1,9 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
 using namespace std;
+
+void Backtracking(vector<string>& Words, vector<bool>& WhenUsed);
